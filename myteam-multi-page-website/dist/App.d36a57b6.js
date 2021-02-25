38788410c55140bf1af4f89218b9d9c2e0732865
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-02-25 17:08:37 -0500

    changed scripts

--- myteam-multi-page-website/dist/App.d36a57b6.js
+++ myteam-multi-page-website/dist/App.d36a57b6.js
@@ -34935,7 +34935,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54401" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55498" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
