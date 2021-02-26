b16b6a3ce9c52f984f5db6785263c8430c3cb692
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-02-26 15:37:26 -0500

    corrected form styles

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -34935,7 +34935,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53792" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49449" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
